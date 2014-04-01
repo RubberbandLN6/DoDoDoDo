13d527d26ba1b1a518fb4bd5abc479da12bf9309
RubberbandLN6 <RubberbandLN6@gmail.com>
2014-04-01 13:18:34 -0700

    4/1/2014
    Fixed adding <li>, added remove button, fade out <li> on click.

--- application.js
+++ application.js
@@ -5,9 +5,34 @@ $(document).ready(function() {
     $('.add').focus();
 
     //add item variable
-
 $('#submit').click(function() {
-    var currentObject = $('#add').val();
-    $("items").append("<li>"+ currentObject + "</li>");
+    var current = $('#add').val();
+    $("#items").append("<li class='new'>"+ current + "<button class='xButton'>X</button></li>");
 });
+//allows enter key to add items
+    $("#add").keyup(function(event){
+        if (event.which === 13) {
+            $("#submit").trigger("click");
+            $("#add").val("");
+        }
+    });
+//show and hide x button
+    $("#items").on("mouseenter", ".new", function() {
+        $(this).find(".xButton").show();
+    });
+
+    $("#items").on("mouseleave", ".new", function() {
+        $(this).find(".xButton").hide();
+    });
+
+//removes items from list
+    $("#items").on("click", ".xButton", function() {
+        $(this).closest("li").remove();
+    });
+//fades out clicked list item
+    $("#items").on("click", ".new", function() {
+        $(this).css({opacity:1.0});
+    }, function(){
+        $(this).css({opacity:0.5});
+    });
 });
